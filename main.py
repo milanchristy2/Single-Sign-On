0e9bbdedfb7da566c170df8a9075ba19bd4194fd
milanchristy2 <milan.christy@bootlabstech.com>
2026-01-19 13:59:43 +0530

    the entrypoint of the application

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+import httpx
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
+import uvicorn
+from routes import router
+
+app=FastAPI()
+
+app.add_middleware(SessionMiddleware,secret_key="secret-123123")
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
